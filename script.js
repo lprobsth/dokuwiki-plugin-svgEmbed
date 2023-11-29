91ede6b43c79f13ab4d13714f3c4024e3186d699
Lukas Probsthain <lukas.probsthain@gmail.com>
2023-11-29 17:18:53 +0100

    add JavaScript function for rendering links in SVGs in the browser (preliminary implementation for lejmr/dokuwiki-plugin-drawio#61

--- script.js
+++ script.js
@@ -24,3 +24,59 @@ function svgembed_onMouseOut(object_id) {
     document.getElementById(object_id).className = document.getElementById(object_id).className.replace(' svgembed_print_border', '');
     return false;
 }
+
+jQuery(function(){
+    function updateSVGLinksInSVG(svgElement) {
+        // Find all <a> elements within each SVG
+        jQuery(svgElement).find('a').each(function() {
+            var $link = jQuery(this);
+            var href = $link.attr('href');
+
+            // Check if the href matches the pattern
+            if (href && href.startsWith('[[') && href.endsWith(']]')) {
+                // Extract namespace, page, and section
+                var parts = href.slice(2, -2).split(':');
+                var namespace = parts[0];
+                var pageSection = parts[1].split('#');
+                var page = pageSection[0];
+                var section = pageSection[1];
+
+                // Construct the new URL (modify this according to your URL structure)
+                var newHref = DOKU_BASE + namespace + '/' + page + (section ? '#' + section : '');
+
+                // Update the href attribute
+                $link.attr('href', newHref);
+
+                $link.attr('target','_blank');
+            }
+        });
+    }
+
+
+    // Function to handle an object element
+    function handleObject($obj) {
+        var svgDoc = $obj[0].contentDocument; // Get the document of the object tag
+        if (svgDoc) {
+            var svg = svgDoc.querySelector('svg');
+            if (svg) {
+                
+                setTimeout(function(){updateSVGLinksInSVG(svg); }, 1000);
+                
+            }
+        }
+    }
+
+    // Find all object tags that are supposed to contain SVGs
+    jQuery('object[type="image/svg+xml"]').each(function() {
+        var $this = jQuery(this);
+
+        // // Check if it's already loaded, or wait for the load event
+        // if ($this[0].contentDocument) {
+        //     handleObject($this);
+        // } else {
+            $this.on('load', function() {
+                handleObject($this);
+            });
+        // }
+    });
+});
